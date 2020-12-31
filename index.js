412de126cdebfa3df21b03ddc97df6a655478795
Rithik Gavvala <gavvalarithik@gmail.com>
2020-12-30 20:24:43 -0500

    test

--- index.js
+++ index.js
@@ -61,7 +61,27 @@ app.command('/help', async ({ command, ack, say }) => {
     // Acknowledge command request
     await ack();
   
-    await say("TEST");
+    await say({
+      blocks: [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": `Hey there <@${message.user}>!`
+          },
+          "accessory": {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Click Me"
+            },
+            "action_id": "button_click"
+          }
+        }
+      ],
+      text: `Hey there <@${message.user}>!`
+    });
+    
     console.log("TEST")
   });
 
